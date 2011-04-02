c1fd3b06c9c1fd8b09daea2683afd12848e36fc1
Elben Shira <elbenshira@gmail.com>
2011-04-02 18:06:54 -0500

    Initial add. Extracted from my Paxos repo.

---
+++ net_controller.py
@@ -0,0 +1,140 @@
+import socket
+from threading import Thread
+import json
+import Queue
+
+class NetController:
+    """
+    UDP. Async, thread-safe queues. Ghetto-simple configuration.
+    What else could you ask for? (Bug-freeness, maybe.)
+
+    Author: Elben Shira <elbenshira@gmail.com>
+
+    Configuration is in JSON format. Here is an example:
+
+        {
+          "procs": [
+            {"ip": "127.0.0.1", "port": 10000},
+            {"ip": "127.0.0.1", "port": 10001},
+            {"ip": "127.0.0.1", "port": 10002}
+          ]
+        }
+
+    For usage example, check out net_controller_tests.py.
+
+    Note that the tests assume we are testing on a local network, thus UDP packets
+    are ordered. This library has not been testing in a non-local environment.
+    """
+
+    def __init__(self, proc_id, config_data=None, config_path='config.json'):
+        self.config = Config(config_data, config_path)
+        self.queue = Queue.Queue()
+        self.proc_id = proc_id
+
+        self.ip = self.config.proc(proc_id)['ip']
+        self.port = self.config.proc(proc_id)['port']
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        # Spawn listener thread.
+        self.shared_state = {'alive': True}
+        self.listener = ListenServer(self.shared_state, self.queue, self.config.proc(self.proc_id)['port'])
+        self.listener.start()
+
+    def send(self, proc_id, msg):
+        """
+        Sends a message to proc_id.
+        """
+
+        payload = self._create_msg(proc_id, msg)
+        self.sock.sendto(payload, self.config.proc_addr(proc_id))
+
+    def send_all(self, msg, exclude_self=True):
+        """
+        Sends a message to all known procs.
+        If exclude_self is True, send it to ourself too.
+        """
+
+        for proc_id, proc in enumerate(self.config.proc_addrs()):
+            if exclude_self and proc_id == self.proc_id:
+                continue
+            payload = self._create_msg(proc_id, msg)
+            self.sock.sendto(payload, tuple(proc))
+
+    def next(self, block=False):
+        """
+        Returns the first (from_addr, msg) tuple in the queue.
+        If `block` is True, this method will block until a message is available.
+        """
+
+        try:
+            addr, payload = self.queue.get(block)
+            return addr, json.loads(payload)
+        except Queue.Empty:
+            return None
+
+    def shutdown(self):
+        self.shared_state['alive'] = False
+        self.listener.join()
+
+    def _create_msg(self, to_proc, msg):
+        payload = json.dumps({
+            'from_proc': self.proc_id,
+            'to_proc': to_proc,
+            'msg': msg},)
+        return payload
+
+class ListenServer(Thread):
+    def __init__(self, shared_state, queue, port, ip='127.0.0.1', timeout=1, *args, **kwargs):
+        Thread.__init__(self, *args, **kwargs)  # must be called first
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.settimeout(timeout)
+        self.queue = queue
+        self.ip = ip
+        self.port = port
+        self.shared_state = shared_state
+
+    def run(self):
+        self.sock.bind((self.ip, self.port))
+        msg = None
+        from_addr = None
+        while self.shared_state['alive']:
+            try:
+                msg, from_addr = self.sock.recvfrom(4096)
+                self.queue.put((from_addr, msg))
+            except socket.timeout:
+                pass
+        self.sock.close()
+
+class Config:
+    def __init__(self, config_data=None, config_path='config.json'):
+        self._procs = []
+        if config_data:
+            config_json = json.loads(config_data)
+        else:
+            config_json = json.load(open(config_path))
+
+        if 'procs' in config_json:
+            self._procs = config_json['procs']
+
+    def procs(self):
+        return self._procs
+
+    def proc_addrs(self):
+        addrs = []
+        for proc in self.procs():
+            addrs.append((proc['ip'], proc['port']))
+        return addrs
+
+    def proc_addr(self, proc_id):
+        proc = self.proc(proc_id)
+        return (proc['ip'], proc['port'])
+
+    def proc(self, proc_id):
+        if len(self._procs) > proc_id:
+            return self._procs[proc_id]
+        else:
+            raise Exception("No process with number %d." % p);
+
+    def num_procs(self):
+        return len(self._procs)
+
---
+++ net_controller_tests.py
@@ -0,0 +1,141 @@
+import time
+import unittest
+from net_controller import *
+
+CONFIG = """
+    {
+      "procs": [
+        {"ip": "127.0.0.1", "port": 10000},
+        {"ip": "127.0.0.1", "port": 10001},
+        {"ip": "127.0.0.1", "port": 10002}
+      ]
+    }
+    """
+
+class TestNetController(unittest.TestCase):
+    def setUp(self):
+        self.nc0 = NetController(0, config_data=CONFIG)
+        self.nc1 = NetController(1, config_data=CONFIG)
+        self.nc2 = NetController(2, config_data=CONFIG)
+
+    def tearDown(self):
+        self.nc0.shutdown()
+        self.nc1.shutdown()
+        self.nc2.shutdown()
+
+    def test_send_all_include_self(self):
+        # Send to all, including to self
+        self.nc0.send_all("send to all", False)
+        self.nc0.send_all("send to all again", False)
+        self.nc0.send_all("again and again and again", False)
+
+        time.sleep(1)
+
+        # Check each net controller got right messages
+
+        addr, payload = self.nc0.next()
+        self.assertEquals(payload['to_proc'], 0)
+        self.assertEquals(payload['from_proc'], 0)
+        self.assertEquals(payload['msg'], 'send to all')
+
+        addr, payload = self.nc0.next()
+        self.assertEquals(payload['msg'], 'send to all again')
+        addr, payload = self.nc0.next()
+        self.assertEquals(payload['msg'], 'again and again and again')
+
+        addr, payload = self.nc1.next()
+        self.assertEquals(payload['to_proc'], 1)
+        self.assertEquals(payload['from_proc'], 0)
+        self.assertEquals(payload['msg'], 'send to all')
+
+        addr, payload = self.nc1.next()
+        self.assertEquals(payload['msg'], 'send to all again')
+        addr, payload = self.nc1.next()
+        self.assertEquals(payload['msg'], 'again and again and again')
+
+    def test_send_all_exclude_self(self):
+        # Send to all, excluding self
+        self.nc0.send_all("send to all")
+        self.nc0.send_all("send to all again")
+        self.nc0.send_all("again and again and again")
+
+        time.sleep(1)
+
+        # Check each net controller got right messages
+
+        data_recv = self.nc1.next()
+        self.assertIsNotNone(data_recv)
+
+        addr, payload = data_recv
+        self.assertEquals(payload['to_proc'], 1)
+        self.assertEquals(payload['from_proc'], 0)
+        self.assertEquals(payload['msg'], 'send to all')
+
+        addr, payload = self.nc1.next()
+        self.assertEquals(payload['msg'], 'send to all again')
+
+        addr, payload = self.nc1.next()
+        self.assertEquals(payload['msg'], 'again and again and again')
+
+        self.assertIsNone(self.nc0.next())
+
+    def test_send(self):
+        # No messages in queue
+        self.assertIsNone(self.nc0.next())
+        self.assertIsNone(self.nc1.next())
+
+        # Send stuff
+        self.nc0.send(1, "test1")
+        self.nc0.send(1, "test2")
+        self.nc1.send(0, "test3")
+        self.nc1.send(0, "test4")
+
+        time.sleep(1)
+
+        # Check each net controller got right messages
+
+        addr, payload = self.nc0.next()
+        self.assertEquals(payload['to_proc'], 0)
+        self.assertEquals(payload['from_proc'], 1)
+        self.assertEquals(payload['msg'], 'test3')
+
+        addr, payload = self.nc0.next()
+        self.assertEquals(payload['to_proc'], 0)
+        self.assertEquals(payload['from_proc'], 1)
+        self.assertEquals(payload['msg'], 'test4')
+
+        addr, payload = self.nc1.next()
+        self.assertEquals(payload['to_proc'], 1)
+        self.assertEquals(payload['from_proc'], 0)
+        self.assertEquals(payload['msg'], 'test1')
+
+        addr, payload = self.nc1.next()
+        self.assertEquals(payload['to_proc'], 1)
+        self.assertEquals(payload['from_proc'], 0)
+        self.assertEquals(payload['msg'], 'test2')
+        
+        # We should run out of messages
+        self.assertIsNone(self.nc0.next())
+        self.assertIsNone(self.nc1.next())
+
+class TestConfig(unittest.TestCase):
+    def setUp(self):
+        self.config = Config(config_data=CONFIG)
+
+    def test_procs(self):
+        procs = self.config.procs()
+        self.assertEquals(procs[0], {"ip": "127.0.0.1", "port": 10000})
+        self.assertEquals(procs[1], {"ip": "127.0.0.1", "port": 10001})
+        self.assertEquals(procs[2], {"ip": "127.0.0.1", "port": 10002})
+
+    def test_proc(self):
+        self.assertEquals(self.config.proc(0), {"ip": "127.0.0.1", "port": 10000})
+        self.assertEquals(self.config.proc(1), {"ip": "127.0.0.1", "port": 10001})
+        self.assertEquals(self.config.proc(2), {"ip": "127.0.0.1", "port": 10002})
+        self.assertRaises(Exception, self.config.proc, (3,))
+
+    def test_num_procs(self):
+        self.assertEquals(self.config.num_procs(), 3)
+
+if __name__ == "__main__":
+    unittest.main()
